cf1b6ed46b91472b3bed873f00967149c5ef6fcf
krushabh-dev <rushabhrbs@gmail.com>
2022-11-22 18:30:26 +0530

    commit-1

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {StyleSheet, ScrollView } from 'react-native';
+import Login from "./Login"
 
-export default function App() {
+const YourApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Login /> 
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default YourApp;
